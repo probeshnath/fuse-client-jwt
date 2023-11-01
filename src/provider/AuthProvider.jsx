a637f7b0c42b5bf0bba3e305bbe797ddeb55e1de
probeshnath <probeshn@gmail.com>
2023-11-01 12:33:34 +0600

    logout user

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,28 +1,39 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import auth from '../../firebase.config'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
 import { GoogleAuthProvider } from "firebase/auth";
 
 export const AuthContext = createContext(null)
 
 const AuthProvider = ({children}) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading]= useState(true)
 
   const googleProvider = new GoogleAuthProvider();
 
   // google login by firebase
   const handleGoogle = () =>{
+    setLoading(true)
     return signInWithPopup(auth,googleProvider)
   }
 
 
   // register user by firebase
   const register = (email,password) =>{
+    setLoading(true)
     return createUserWithEmailAndPassword(auth, email,password)
   }
 
   // login user by firebase
   const login = (email,password)=>{
+    setLoading(true)
     return signInWithEmailAndPassword(auth,email,password)
+  }
+
+  // logout user
+  const logoutUser = () =>{
+    setLoading(true)
+    return signOut(auth)
   }
 
 
@@ -30,8 +41,12 @@ const AuthProvider = ({children}) => {
         name: "probesh User's",
         register,
         login,
-        handleGoogle
+        handleGoogle,
+        logoutUser
     }
+
+
+
   return (
     <AuthContext.Provider value={authInfo}>
         {children}
