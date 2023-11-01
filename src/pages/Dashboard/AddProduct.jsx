7d3d4984bdb16e7b7939644fcbf4ccaf3705b7fa
probeshnath <probeshn@gmail.com>
2023-11-01 16:03:48 +0600

    add producut and update product page added

--- src/pages/Dashboard/AddProduct.jsx
+++ src/pages/Dashboard/AddProduct.jsx
@@ -1,8 +1,60 @@
+import axios from 'axios';
 import React from 'react'
 
 const AddProduct = () => {
+  const addProduct = (e) =>{
+    e.preventDefault();
+    // console.log("create product")
+    const form = e.target;
+    const pName = form.productName.value;
+    const price = form.price.value;
+    const img = form.productImg.value;
+
+    const product = {pName,price,img}
+    console.log(product)
+
+    // axios.post("/dashboard/products",product)
+    // .then((result)=>{
+    //   console.log(result)
+    // })
+
+
+
+  }
   return (
-    <div>AddProduct</div>
+    <div>
+      <h2 className='text-center text-5xl text-orange-600 font-bold'>Add Product</h2>
+       <form onSubmit={addProduct} className="card-body">
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Product Name</span>
+          </label>
+          <input name='productName' type="text" placeholder="Product Name" className="input input-bordered" required />
+        </div>
+        
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Price</span>
+          </label>
+          <input type="text" name='price' placeholder="Product Price" className="input input-bordered" required />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Product Image</span>
+          </label>
+          <input type="text" name='productImg' placeholder="Product Image" className="input input-bordered"  />
+        </div>
+
+        
+
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Create Product</button>
+        </div>
+        
+      </form>
+    </div>
   )
 }
 
--- src/pages/Dashboard/UpdateProduct.jsx
+++ src/pages/Dashboard/UpdateProduct.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 const UpdateProduct = () => {
   return (
-    <div>UpdateProduct</div>
+    <div>
+        <h2 className='text-center text-5xl text-orange-600 font-bold'>Update Product</h2>
+       <form onSubmit={addProduct} className="card-body">
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Product Name</span>
+          </label>
+          <input name='productName' type="text" placeholder="Product Name" className="input input-bordered" required />
+        </div>
+        
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Price</span>
+          </label>
+          <input type="text" name='price' placeholder="Product Price" className="input input-bordered" required />
+        </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Product Image</span>
+          </label>
+          <input type="text" name='productImg' placeholder="Product Image" className="input input-bordered"  />
+        </div>
+
+        
+
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Create Product</button>
+        </div>
+        
+      </form>
+    </div>
   )
 }
 
