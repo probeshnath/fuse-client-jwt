7ffbf6f41a39c757a123023d5d003beba74bb248
probeshnath <probeshn@gmail.com>
2023-11-01 11:56:36 +0600

    login user by firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../provider/AuthProvider'
 
 const Login = () => {
+  const {login} = useContext(AuthContext);
+  const navigate = useNavigate()
+
+  const handleLogin = (e) =>{
+    e.preventDefault()
+    console.log("first")
+    const form = e.target;
+    
+    const email = form.email.value;
+    const password = form.password.value;
+
+    const user = {email,password}
+    // console.log(user)
+
+    // firebase register
+    login(email,password)
+    .then(result => {
+      console.log(result.user)
+      navigate("/")
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
   return (
     <div>
          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
+      <form onSubmit={handleLogin} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input name='email' type="email" placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" name='password' placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,22 +1,27 @@
 import React, { createContext } from 'react'
 import auth from '../../firebase.config'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 
 export const AuthContext = createContext(null)
 
 const AuthProvider = ({children}) => {
 
 
-  // register user in firebase
-
+  // register user by firebase
   const register = (email,password) =>{
     return createUserWithEmailAndPassword(auth, email,password)
+  }
+
+  // login user by firebase
+  const login = (email,password)=>{
+    return signInWithEmailAndPassword(auth,email,password)
   }
 
 
     const authInfo = {
         name: "probesh User's",
-        register
+        register,
+        login
     }
   return (
     <AuthContext.Provider value={authInfo}>
